68890df2decb22582e115958ec4af62acc6eec8d
Eytan123123 <119526687+Eytan123123@users.noreply.github.com>
2023-06-25 17:06:59 +0300

    trying shuffle bord

--- src/main/java/frc/robot/logging/DriveTrainShuffleboard.java
+++ src/main/java/frc/robot/logging/DriveTrainShuffleboard.java
@@ -3,19 +3,20 @@ package frc.robot.logging;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.subsystems.driveTrain.DriveTrain;
+import frc.robot.subsystems.driveTrain.DriveTrainComponents;
 
 public class DriveTrainShuffleboard {
 
     private DriveTrain driveTrain;
     private ShuffleboardTab tab;
 
-    public DriveTrainShuffleboard() {
+    public DriveTrainShuffleboard(DriveTrainComponents components) {
         driveTrain = DriveTrain.getInstance();
 
         tab = Shuffleboard.getTab("DriveTrain");
 
         tab.addNumber("motorCurrent", driveTrain::getCurrent);
 
-        //tab.addDouble("EncoderCounter", Swerve.getinstance.getEncoderUnits);
+        //tab.addDouble("EncoderCounter", components.getLeftMasterMotor().);
     }
 }
