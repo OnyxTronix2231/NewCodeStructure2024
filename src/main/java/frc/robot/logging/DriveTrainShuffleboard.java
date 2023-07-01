7b60b6fe79c2f21d582a6b18d5527fcee58a0671
Eytan123123 <eytanb007@gmail.com>
2023-07-01 16:27:24 +0300

    starting ShuffleBord

--- src/main/java/frc/robot/logging/DriveTrainShuffleboard.java
+++ src/main/java/frc/robot/logging/DriveTrainShuffleboard.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.subsystems.driveTrain.DriveTrain;
 import frc.robot.subsystems.driveTrain.DriveTrainComponents;
+import frc.robot.subsystems.driveTrain.DriveTrainComponentsImpl;
 
 public class DriveTrainShuffleboard {
 
@@ -14,9 +15,11 @@ public class DriveTrainShuffleboard {
         driveTrain = DriveTrain.getInstance();
 
         tab = Shuffleboard.getTab("DriveTrain");
+        tab.addNumber("encoder units ", () -> driveTrain.getSwerveModulesPosition()[0].angle.getRadians());
 
-        tab.addNumber("motorCurrent", driveTrain::getCurrent);
-
+        tab.addNumber("robot's angle ", () -> )
+        //זווית של הרובוט, זווית של כל מודלוה, משהו מהאינקודר, סיבוב של 90 מעלות.
+        //של לדים: להציג צבע נוכחי, לנחליף לאדום, להחליף לרייבואואוואואואואואואו
         //tab.addDouble("EncoderCounter", components.getLeftMasterMotor().);
     }
 }
