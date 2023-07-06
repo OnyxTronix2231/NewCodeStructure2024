f0a0462fcc20cde21cec01b60982f276c1a23a2f
negbi2 <itzhakn@protonmail.com>
2023-07-06 13:38:58 +0300

    made a command to light the leds in a rainbow pattern

--- src/main/java/frc/robot/led/LED.java
+++ src/main/java/frc/robot/led/LED.java
@@ -8,8 +8,9 @@ import static frc.robot.led.LEDConstants.RAINBOW_VALUE;
 import static frc.robot.led.LEDConstants.STRIP_LENGTH;
 
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LED {
+public class LED extends SubsystemBase {
 
     private Color currentColor;
 
