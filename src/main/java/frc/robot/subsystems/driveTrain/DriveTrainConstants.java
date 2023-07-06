c356a83f2a35aa1943da6f3476b6425cb8167c06
ItsNotMe-jpg <yoni.071007@gmail.com>
2023-07-06 19:01:40 +0300

    morehsmae

--- src/main/java/frc/robot/subsystems/driveTrain/DriveTrainConstants.java
+++ src/main/java/frc/robot/subsystems/driveTrain/DriveTrainConstants.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems.driveTrain;
 public class DriveTrainConstants {
 
     public static final class ComponentsConstants {
-        public static int RIGHT_PORT = 1;
-        public static int LEFT_PORT = 2;
-        public static int LEFT_SALVE_PORT = 4;
-        public static int RIGHT_SLAVE_PORT = 3;
+        public static int RIGHT_Motor_PORT = 1;
+        public static int LEFT_Motor_PORT = 2;
+        public static int LEFT_SALVE_Motor_PORT = 4;
+        public static int RIGHT_SLAVE_Motor_PORT = 3;
     }
 }
