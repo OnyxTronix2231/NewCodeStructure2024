15938c23b9862ec7eaaade7172253de77756984f
benalaluf <94129183+benalaluf@users.noreply.github.com>
2023-07-06 19:31:50 +0300

    space

--- src/main/java/frc/robot/subsystems/driveTrain/DriveTrainConstants.java
+++ src/main/java/frc/robot/subsystems/driveTrain/DriveTrainConstants.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.driveTrain;
 public class DriveTrainConstants {
 
     static final class ComponentsConstants{
+
         static final int LEFT_MASTER_MOTOR_PORT = 2;
         static final int LEFT_SLAVE_MOTOR_PORT = 4;
         static final int RIGHT_MASTER_MOTOR_PORT = 1;
