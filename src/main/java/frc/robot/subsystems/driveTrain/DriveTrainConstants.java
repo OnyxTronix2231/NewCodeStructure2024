0fd84b7425472456926d35ace8e0abddf7882868
benalaluf <94129183+benalaluf@users.noreply.github.com>
2023-07-06 19:31:30 +0300

    remove red

--- src/main/java/frc/robot/subsystems/driveTrain/DriveTrainConstants.java
+++ src/main/java/frc/robot/subsystems/driveTrain/DriveTrainConstants.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems.driveTrain;
 public class DriveTrainConstants {
 
     static final class ComponentsConstants{
-        static final int LEFT_MASTER_MOTOR_PORT = 1;
-        static final int LEFT_SLAVE_MOTOR_PORT = 3;
-        static final int RIGHT_MASTER_MOTOR_PORT = 2;
-        static final int RIGHT_SLAVE_MOTOR_PORT = 5;
+        static final int LEFT_MASTER_MOTOR_PORT = 2;
+        static final int LEFT_SLAVE_MOTOR_PORT = 4;
+        static final int RIGHT_MASTER_MOTOR_PORT = 1;
+        static final int RIGHT_SLAVE_MOTOR_PORT = 3;
     }
 }
