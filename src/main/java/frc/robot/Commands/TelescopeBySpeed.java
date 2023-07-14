156af73d4835f8b6d0067e8ae123e229871ae55c
AmirHoffman1 <amirik.hoffman@gmail.com>
2023-07-14 06:44:47 -0700

    Update TelescopeBySpeed.java

--- src/main/java/frc/robot/Commands/TelescopeBySpeed.java
+++ src/main/java/frc/robot/Commands/TelescopeBySpeed.java
@@ -2,7 +2,6 @@ package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.telescope.Telescope;
-
 import java.util.function.DoubleSupplier;
 
 public class TelescopeBySpeed extends CommandBase {
