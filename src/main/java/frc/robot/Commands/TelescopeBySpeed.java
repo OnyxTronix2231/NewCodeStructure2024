51cf08d7484943684498f64114351df0f61dcf34
AmirHoffman1 <amirik.hoffman@gmail.com>
2023-07-14 05:36:46 -0700

    Create TelescopeBySpeed.java

---
+++ src/main/java/frc/robot/Commands/TelescopeBySpeed.java
@@ -0,0 +1,31 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.telescope.Telescope;
+
+import java.util.function.DoubleSupplier;
+
+public class TelescopeBySpeed extends CommandBase {
+    private Telescope telescope;
+    private DoubleSupplier speedSupplier;
+
+    public TelescopeBySpeed(DoubleSupplier speedSupplier) {
+        this.telescope = Telescope.getInstance();
+        this.speedSupplier = speedSupplier;
+    }
+
+    @Override
+    public void initialize() {
+        telescope.setSpeed(speedSupplier.getAsDouble());
+    }
+
+    @Override
+    public void execute() {
+        telescope.setSpeed(speedSupplier.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        telescope.stop();
+    }
+}
