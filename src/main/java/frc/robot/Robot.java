78105f60dc115083c6d4f3244bd995797d139edb
negbi <itzhakn@protonmail.com>
2023-07-18 17:46:35 +0300

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.test.LedShuffleboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -20,6 +21,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
+        LedShuffleboard.init();
     }
 
     /**
